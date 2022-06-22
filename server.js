8e9732db2fb1a1c68da470acca1e08cd25574c36
jrosent8 <86493608+jrosent@users.noreply.github.com>
2022-06-22 15:38:21 -0700

    Change http to https server. Add axios and cheerio to npm.

--- server.js
+++ server.js
@@ -1,11 +1,30 @@
+//Import Environment Variables
 require('dotenv').config();
+
+//Node modules
+const https = require('https');
+const path = require('path');
+const fs = require('fs');
+
+//Installed modules
+const axios = require('axios');
+const cheerio = require('cheerio');
 const express = require('express');
 
+//Run Express application
 const app = express();
+
+//Assign environment variableshj
 const ROOT_DIR = process.env.ROOT_DIR;
 const PORT = process.env.PORT;
+const KEY = process.env.KEY_LOC;
+const CERT = process.env.CERT_LOC;
 
-app.listen(PORT);
+https.createServer({
+    key: fs.readFileSync(path.join(__dirname, KEY)),
+    cert: fs.readFileSync(path.join(__dirname,CERT))
+},app)
+.listen(PORT, () => console.log("Listening on port: " + PORT));
 
 //Main Page
 app.get("/",(req,res) => {
