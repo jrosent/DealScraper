ac4b1120dbae8c5e8204ed5d5973f699f312269d
jrosent8 <86493608+jrosent@users.noreply.github.com>
2022-07-01 19:16:04 -0700

    Add conditional if search finds no results.

--- scrape.js
+++ scrape.js
@@ -7,24 +7,30 @@ async function tester(){
     const response = await fetch(endpoint);
     const title = await response.json();
     const deals = title.deals;
+    const root = document.getElementById('root');
 
-    deals.forEach(element => {
-        let div = document.createElement('div');
-        div.id = 'deal';
+    if(deals.length <= 0){
+        root.innerHTML = "NO RESULTS FOUND FOR '" + search + "'";
+    }
+    else{
+        deals.forEach(element => {
+            let div = document.createElement('div');
+            div.id = 'deal';
 
-        let imgA = document.createElement('a');
-        imgA.href = element.link;
+            let imgA = document.createElement('a');
+            imgA.href = element.link;
 
-        let img = document.createElement('img');
-        img.src = element.img;
+            let img = document.createElement('img');
+            img.src = element.img;
 
-        let d = document.createElement('a');
-        d.innerHTML = element.title;
-        d.href = element.link;
+            let d = document.createElement('a');
+            d.innerHTML = element.title;
+            d.href = element.link;
 
-        imgA.appendChild(img);
-        div.appendChild(imgA);
-        div.appendChild(d);
-        document.body.appendChild(div);
-    });
+            imgA.appendChild(img);
+            div.appendChild(imgA);
+            div.appendChild(d);
+            root.appendChild(div);
+        });
+    }
 }
