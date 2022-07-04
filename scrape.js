26e24f478603c3ae2e1bf55c3534349995e1e53a
jrosent8 <86493608+jrosent@users.noreply.github.com>
2022-07-03 21:23:16 -0700

    Get the url of the document to send request to server.

--- scrape.js
+++ scrape.js
@@ -1,8 +1,9 @@
 
 async function tester(){
     //let msg = document.createElement('p');
+    let url = document.URL;
     let search = document.getElementById('searchText').value;
-    let endpoint = new URL('http://localhost:3000/scrapeDeals');
+    let endpoint = new URL(url + 'scrapeDeals');
     endpoint.searchParams.set('searchText',search);
     const response = await fetch(endpoint, {});
     const title = await response.json();
