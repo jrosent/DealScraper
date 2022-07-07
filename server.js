3aaf23c8acbd124cf787958f38d291c86ecef126
jrosent8 <86493608+jrosent@users.noreply.github.com>
2022-07-06 21:44:18 -0700

    Use puppeteer for scraping instead of axios in server.js.

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const url = require('url');
 const axios = require('axios');
 const cheerio = require('cheerio');
 const express = require('express');
+const puppeteer = require('puppeteer');
 
 
 //Run Express application
@@ -19,8 +20,6 @@ const app = express();
 //Assign environment variables
 const ROOT_DIR = process.env.ROOT_DIR;
 const PORT = process.env.PORT;
-const KEY = process.env.KEY_LOC;
-const CERT = process.env.CERT_LOC;
 
 http.createServer(app)
 .listen(PORT, () => console.log("Listening on port: " + PORT));
@@ -37,12 +36,13 @@ app.get("/scrape.js",(req,res) => {
 
 app.get("/scrapeDeals", async (req,res) => {
 
-    const title = await scrapeDeals(req.query.searchText);
+    const title = await puppetScrape(req.query.searchText);
     res.header("Access-Control-Allow-Origin","*");
     res.send(title);
 
 })
 
+//Scrapes static html from given url
 async function scrapeDeals(searchText){
     const URL = process.env.NE;
 
@@ -64,4 +64,38 @@ async function scrapeDeals(searchText){
   
     let jsonObj = {deals};
     return jsonObj;
+}
+
+//Scrape website for deal items using puppeteer to load scripts
+async function puppetScrape(searchText) {
+  const browser = await puppeteer.launch();
+
+  const page = await browser.newPage();
+  await page.goto(process.env.NE);
+
+  const htmlData = await page.evaluate(() => {
+    return {
+      data: document.documentElement.innerHTML,
+    };
+  });
+
+  const body = htmlData.data;
+  let $ = cheerio.load(body);
+  let deals = [];
+
+  $('#shopAllDealsItemsWithPaging').find(process.env.NE_CELL).each((_, e) => {
+      let title = $(e).find(process.env.NE_TITLE).text();
+      if (title.toLowerCase().includes(searchText.toLowerCase()) || title === '') {
+        let img = $(e).find(process.env.NE_IMG).find('img').attr('src');
+        let link = $(e).find(process.env.NE_TITLE).attr('href');
+        let price = $(e).find(process.env.NE_PRICE).text();
+        deals.push({ img: img, title: title, link: link, price: price });
+      };
+    });
+
+  let jsonObj = { deals };
+
+  await browser.close();
+
+  return jsonObj;
 }
