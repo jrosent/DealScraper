3071c50c2273b38515c00041482e131fb32c4eb8
jrosent8 <86493608+jrosent@users.noreply.github.com>
2022-07-09 13:59:44 -0700

    Move scraping and filtering logic to seperate directory.

---
+++ Scrape/scLogic.js
@@ -0,0 +1,93 @@
+import * as fs from "fs";
+
+//Scrapes static html from given url
+async function scrapeDeals(searchText){
+    const URL = process.env.NE;
+
+    const htmlData = await axios(URL);
+    const body = htmlData.data;
+    let $ = cheerio.load(body);
+    let deals = [];
+    let i = 0;
+        
+    $(process.env.NE_CELL).each((_,e)=>{
+        let title = $(e).find(process.env.NE_TITLE).text();
+        if(title.toLowerCase().includes(searchText.toLowerCase()) || title === ''){
+            let img = $(e).find(process.env.NE_IMG).find('img').attr('src');
+            let link = $(e).find(process.env.NE_TITLE).attr('href');
+            let price = $(e).find(process.env.NE_PRICE).text();
+            deals.push({'img':img, 'title':title, 'link':link, 'price':price});
+        }
+    });
+  
+    let jsonObj = {deals};
+    return jsonObj;
+}
+
+//Scrape website for deal items using puppeteer to load scripts
+async function filterBySearch(searchText,res) {
+    fs.readFile("./neScrapeData.json",'utf8', (err, data)=>{
+        if(err){
+            return;
+        }
+        else{
+            const resp = {'deals':[]};
+            const jsonObj = JSON.parse(data);
+            jsonObj.deals.forEach((element)=>{
+                if (element.title.toLowerCase().includes(searchText.toLowerCase()) 
+                || element.title === ''){
+                    resp.deals.push(element);
+                };
+            });
+            res.header("Access-Control-Allow-Origin","*");
+            res.send(resp);
+        };
+    });
+};
+
+//Reads from browser and writes information to json file
+async function scrapeToFile() {
+    const chromeOptions = {
+        headless:false,
+        defaultViewPort: null
+    };
+
+    const browser = await puppeteer.launch(chromeOptions);
+
+    const page = await browser.newPage()
+    await page.goto(process.env.NE)
+
+    const htmlData = await page.evaluate(() => {
+        return {
+            data: document.documentElement.innerHTML,
+        }
+    })
+
+    const body = htmlData.data
+    let $ = cheerio.load(body)
+    let deals = []
+
+    $('#shopAllDealsItemsWithPaging')
+        .find(process.env.NE_CELL)
+        .each((_, e) => {
+            let title = $(e).find(process.env.NE_TITLE).text();
+            let img = $(e).find(process.env.NE_IMG).find('img').attr('src');
+            let link = $(e).find(process.env.NE_TITLE).attr('href');
+            let price = $(e).find(process.env.NE_PRICE).text();
+            deals.push({ img: img, title: title, link: link, price: price });
+        });
+
+    let jsonObj = {deals};
+    await browser.close();
+
+    fs.writeFile("./neScrapeData.json", JSON.stringify(jsonObj),(err)=>{
+        if(err){
+            console.log(err);
+        }
+        else{
+            console.log("File written successfully");
+        };
+    });
+}
+
+export {filterBySearch as filterBySearch, scrapeToFile as scrapeToFile};
--- server.js
+++ server.js
@@ -12,6 +12,7 @@ import axios from "axios";
 import cheerio from "cheerio";
 import express, { json } from "express";
 import puppeteer from "puppeteer";
+import {scrapeToFile,filterBySearch} from "./Scrape/scLogic.js"
 
 //Run Express application
 const app = express();
@@ -40,117 +41,8 @@ app.get("/client.js",(req,res) => {
 
 
 app.get("/scrapeDeals", async (req,res) => {
-    //await filterBySearch(req.query.searchText);
-    const searchText = req.query.searchText;
-    fs.readFile("./neScrapeData.json",'utf8', (err, data)=>{
-        if(err){
-            console.log(err);
-        }
-        else{
-            const resp = {'deals':[]};
-            const jsonObj = JSON.parse(data);
-            jsonObj.deals.forEach((element)=>{
-                if (element.title.toLowerCase().includes(searchText.toLowerCase()) 
-                || element.title === ''){
-                    resp.deals.push(element);
-                };
-            });
-            res.header("Access-Control-Allow-Origin","*");
-            res.send(resp);
-            res.end();
-        };
-    });
+    await filterBySearch(req.query.searchText,res);
     //console.log(JSON.stringify(response));
     //res.header("Access-Control-Allow-Origin","*");
     //res.end();
 })
-
-
-//Scrapes static html from given url
-async function scrapeDeals(searchText){
-    const URL = process.env.NE;
-
-    const htmlData = await axios(URL);
-    const body = htmlData.data;
-    let $ = cheerio.load(body);
-    let deals = [];
-    let i = 0;
-        
-    $(process.env.NE_CELL).each((_,e)=>{
-        let title = $(e).find(process.env.NE_TITLE).text();
-        if(title.toLowerCase().includes(searchText.toLowerCase()) || title === ''){
-            let img = $(e).find(process.env.NE_IMG).find('img').attr('src');
-            let link = $(e).find(process.env.NE_TITLE).attr('href');
-            let price = $(e).find(process.env.NE_PRICE).text();
-            deals.push({'img':img, 'title':title, 'link':link, 'price':price});
-        }
-    });
-  
-    let jsonObj = {deals};
-    return jsonObj;
-}
-
-//Scrape website for deal items using puppeteer to load scripts
-async function filterBySearch(searchText) {
-    fs.readFile("./neScrapeData.json",'utf8', (err, data)=>{
-        if(err){
-            console.log(err);
-        }
-        else{
-            const resp = {'deals':[]};
-            const jsonObj = JSON.parse(data);
-            jsonObj.deals.forEach((element)=>{
-                if (element.title.toLowerCase().includes(searchText.toLowerCase()) 
-                || element.title === ''){
-                    resp.deals.push(element);
-                };
-            });
-            res.header("Access-Control-Allow-Origin","*");
-            res.send(resp);
-        };
-    });
-};
-
-async function scrapeToFile() {
-    const chromeOptions = {
-        headless:false,
-        defaultViewPort: null
-    };
-
-    const browser = await puppeteer.launch(chromeOptions);
-
-    const page = await browser.newPage()
-    await page.goto(process.env.NE)
-
-    const htmlData = await page.evaluate(() => {
-        return {
-            data: document.documentElement.innerHTML,
-        }
-    })
-
-    const body = htmlData.data
-    let $ = cheerio.load(body)
-    let deals = []
-
-    $('#shopAllDealsItemsWithPaging')
-        .find(process.env.NE_CELL)
-        .each((_, e) => {
-            let title = $(e).find(process.env.NE_TITLE).text();
-            let img = $(e).find(process.env.NE_IMG).find('img').attr('src');
-            let link = $(e).find(process.env.NE_TITLE).attr('href');
-            let price = $(e).find(process.env.NE_PRICE).text();
-            deals.push({ img: img, title: title, link: link, price: price });
-        });
-
-    let jsonObj = {deals};
-    await browser.close();
-
-    fs.writeFile("./neScrapeData.json", JSON.stringify(jsonObj),(err)=>{
-        if(err){
-            console.log(err);
-        }
-        else{
-            console.log("File written successfully");
-        };
-    });
-}
